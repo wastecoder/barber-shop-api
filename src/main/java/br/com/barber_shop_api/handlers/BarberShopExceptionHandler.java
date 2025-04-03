7858eaf24d91eee264890a5c36a6ee8a183f91b8
Paulino <paulinomendes03@gmail.com>
2025-04-03 07:27:00 -0300

    feat(ExceptionHandler): adiciona tratamento de erro para os @Valid

--- src/main/java/br/com/barber_shop_api/handlers/BarberShopExceptionHandler.java
+++ src/main/java/br/com/barber_shop_api/handlers/BarberShopExceptionHandler.java
@@ -3,14 +3,18 @@ package br.com.barber_shop_api.handlers;
 import br.com.barber_shop_api.controllers.response.ProblemResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.OffsetDateTime;
+import java.util.stream.Collectors;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Log4j2
@@ -18,7 +22,7 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 public class BarberShopExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleUncaught(final Exception ex, final WebRequest request){
+    public ResponseEntity<?> handleUncaught(final Exception ex, final WebRequest request) {
         log.error("handleUncaught: ", ex);
         var status = INTERNAL_SERVER_ERROR;
         var response = ProblemResponse.builder()
@@ -29,4 +33,27 @@ public class BarberShopExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, response, new HttpHeaders(), status, request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        log.warn("Validation error: {}", ex.getMessage());
+
+        // Concatena os erros em uma única string
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        var response = ProblemResponse.builder()
+                .status(BAD_REQUEST.value())
+                .timestamp(OffsetDateTime.now())
+                .message(errorMessage)
+                .build();
+
+        return handleExceptionInternal(ex, response, headers, BAD_REQUEST, request);
+    }
 }
+

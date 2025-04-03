0fa4511e7148e3c279545879ab5f81334f4247fb
Paulino <paulinomendes03@gmail.com>
2025-04-03 02:39:38 -0300

    feat: início do projeto

---
+++ src/test/java/br/com/barber_shop_api/BarberShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.barber_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
